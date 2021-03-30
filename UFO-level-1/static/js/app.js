1d23878b816bc04f01ab6952904e41c77e725fb7
Maria Valdes <maria.v.valdes@gmail.com>
2021-03-29 20:14:31 -0400

    added filter

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -6,7 +6,7 @@ var tableData = data;
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 
-// Console.log the weather data from data.js
+// Console.log the ufodata from data.js
 console.log(data);
 
 // Step 1: Loop Through `data` and console.log each ufodata object
@@ -27,3 +27,75 @@ console.log(data);
       cell.text(value);
     });
   });
+
+
+// Select the button
+var button = d3.select("#filter-btn");
+
+// Select the form
+var form = d3.select(".form-control");
+
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit", runEnter);
+form.addEventListener('keyup', function(event) {
+    if (event.code === 'Enter') {
+      runEnter();
+    }
+  });
+
+function runEnter() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+
+  // get the value from the form
+
+  var inputValue = form.property("value");
+
+//  filter table by date
+
+var filteredData = tableData.filter(table => table.datetime === inputValue);
+
+console.log(filteredData);
+
+// clear original table
+tbody.html("");
+
+//display filtered table
+
+filteredData.forEach(function(filter) {
+  console.log(filter);
+
+// Step 2:  Use d3 to append one table row `tr` for each ufodata object
+  var row = tbody.append("tr");
+// Step 3:  Use `Object.entries` to console.log each ufodata value
+  Object.entries(filter).forEach(function([key, value]) {
+    console.log(key, value);
+
+// Step 4: Use d3 to append 1 cell per ufodata value
+    var cell = row.append("td");
+
+// Step 5: Use d3 to update each cell's text with ufodata values
+    cell.text(value);
+  });
+});
+
+
+
+
+
+  // // Select the input element and get the raw HTML node
+  // var  = d3.select("#patient-form-input");
+
+  // // Get the value property of the input element
+  // var inputValue = inputElement.property("value");
+
+  // console.log(inputValue);
+  // console.log(people); 
+
+  // var filteredData = people.filter(person => person.bloodType === inputValue);
+
+  // console.log(filteredData);
+
+};
